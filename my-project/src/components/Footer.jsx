9f9c66f5dcb195a71268d5a8184c01a02debb2fe
divya <divyaraj172006@gmail.com>
2026-02-11 12:49:37 +0530

    minichatgpt

---
+++ my-project/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer style={{ padding: '1rem', backgroundColor: '#ffffffff', marginTop: '1rem' }}>
+            <h1>Footer</h1>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
